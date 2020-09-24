b11850579f496b5f2027f702aa36dac9fd64c2cf
leks <kurbakov.av@gmail.com>
2020-09-24 21:24:38 +0300

    init

---
+++ src/main/java/com/example/ushort/UshortApplication.java
@@ -0,0 +1,11 @@
+package com.example.ushort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshortApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UshortApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ushort/UshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ushort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
